046ac6b7f5f1ed99048972f649963416bc5e33cb
AndersVad <anders.vad.nielsen@gmail.com>
2020-02-26 08:14:06 +0100

    Basic snap/stick/dock/collapse features implemented. (#1)
    * Added basic content control.
    
    * Failed attempt.
    
    * Hacked early solution for snapping/sticking left side.
    
    * Snap/stick on top and sides for single monitor achieved. Still needs a lot of refactoring.
    
    * Updated dockstates on mouse up/down.
    
    * added collapsing control.
    
    * renamed dockhost to dockhost1.
    
    * Refactored DockHost.
    
    * Added dropshadow.
    
    * Fixed shadow bug.
    
    * clean up.

---
+++ src/WpfApp1/Monitors.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp1
+{
+  //https://stackoverflow.com/a/35681472
+  /// <summary>
+  /// This class deals with monitors.
+  /// </summary>
+  internal static class Monitors
+  {
+    private static List<Screen> screens;
+
+    internal static List<Screen> GetScreens()
+    {
+      screens = new List<Screen>();
+
+      var handler = new NativeMethods.DisplayDevicesMethods.EnumMonitorsDelegate(MonitorEnumProc);
+      NativeMethods.DisplayDevicesMethods.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, handler, IntPtr.Zero); // should be sequential
+
+      return screens;
+    }
+
+    private static bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, NativeMethods.DisplayDevicesMethods.RECT rect, IntPtr dwData)
+    {
+      NativeMethods.DisplayDevicesMethods.MONITORINFO mi = new NativeMethods.DisplayDevicesMethods.MONITORINFO();
+
+      if (NativeMethods.DisplayDevicesMethods.GetMonitorInfo(hMonitor, mi))
+      {
+        screens.Add(new Screen(
+          (mi.dwFlags & 1) == 1, // 1 = primary monitor
+          mi.rcMonitor.Left,
+          mi.rcMonitor.Top,
+          Math.Abs(mi.rcMonitor.Right - mi.rcMonitor.Left),
+          Math.Abs(mi.rcMonitor.Bottom - mi.rcMonitor.Top)));
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// Represents a display device on a single system.
+    /// </summary>
+    internal sealed class Screen
+    {
+      /// <summary>
+      /// Initializes a new instance of the Screen class.
+      /// </summary>
+      /// <param name="primary">A value indicating whether the display is the primary screen.</param>
+      /// <param name="x">The display's top corner X value.</param>
+      /// <param name="y">The display's top corner Y value.</param>
+      /// <param name="w">The width of the display.</param>
+      /// <param name="h">The height of the display.</param>
+      internal Screen(bool primary, int x, int y, int w, int h)
+      {
+        IsPrimary = primary;
+        TopX = x;
+        TopY = y;
+        Width = w;
+        Height = h;
+      }
+
+      /// <summary>
+      /// Gets a value indicating whether the display device is the primary monitor.
+      /// </summary>
+      internal bool IsPrimary { get; }
+
+      /// <summary>
+      /// Gets the display's top corner X value.
+      /// </summary>
+      internal int TopX { get; }
+
+      /// <summary>
+      /// Gets the display's top corner Y value.
+      /// </summary>
+      internal int TopY { get; }
+
+      /// <summary>
+      /// Gets the width of the display.
+      /// </summary>
+      internal int Width { get; }
+
+      /// <summary>
+      /// Gets the height of the display.
+      /// </summary>
+      internal int Height { get; }
+    }
+  }
+}
